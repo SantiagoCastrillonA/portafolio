6f99b522eba5a19e21415164c37a0b45aae495c6
SantiagoCA <satiago.castrilloa@uqvirtual.edu.co>
2024-11-15 15:30:46 -0500

    solución del menú hamburguesa en móviles

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ document.addEventListener('DOMContentLoaded', function() {
         fadeOutDelay: 500
     });
 });
+
+
+// Inicializa EmailJS
+(function() {
+    emailjs.init("your_user_id"); // Reemplaza "your_user_id" con tu User ID de EmailJS
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Captura los datos del formulario
+    const from_email = event.target.from_email.value;
+    const subject = event.target.subject.value;
+    const message = event.target.message.value;
+
+    // Enviar el formulario a través de EmailJS
+    emailjs.send("service_hqbu5kf", "your_template_id", { // Reemplaza "your_template_id" con tu Template ID
+        from_email: from_email,
+        subject: subject,
+        message: message
+    })
+    .then(function(response) {
+        alert("Mensaje enviado con éxito!");
+    }, function(error) {
+        alert("Hubo un error al enviar el mensaje. Inténtalo nuevamente.");
+    });
+});
