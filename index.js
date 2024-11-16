ef6708c0ce82cb8ff5ba97f813d6a91c7d85fff1
SantiagoCA <satiago.castrilloa@uqvirtual.edu.co>
2024-11-16 09:38:54 -0500

    correccion del typed del index.html

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-  new Typed("#typed", {
-    stringsElement: "#typed-strings",
-    typeSpeed: 50, // Velocidad de escritura
-    backSpeed: 30, // Velocidad de borrado
-    startDelay: 1000, // Retardo antes de empezar
-    backDelay: 1500, // Tiempo de espera antes de borrar
-    loop: true, // Hacer que se repita
-    showCursor: true, // Mostrar el cursor parpadeante
-    cursorChar: "|", // Carácter del cursor
-    autoInsertCss: true, // Insertar CSS automáticamente
-    fadeOut: true, // Efecto de desvanecimiento
-    fadeOutClass: "typed-fade-out",
-    fadeOutDelay: 500,
-  });
-  });
-*/
-
 document.addEventListener("DOMContentLoaded", () => {
     // Inicialización del efecto typing
     if (!window.typed) {
